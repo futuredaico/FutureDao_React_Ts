f6b98d465ff6cb66d4d4e47e999a2f23d6e7ed43
dabanli123 <1031737848@qq.com>
2020-01-15 12:50:36 +0800

    更新请求url

--- src/containers/projectinfo/molochdao/info/detail.tsx
+++ src/containers/projectinfo/molochdao/info/detail.tsx
@@ -31,16 +31,12 @@ class MolochDetail extends React.Component<IMolochInfoProps, IState> {
     }
     public render()
     {
-        if (!this.props.molochinfo.projInfo)
-        {
-            return null;
-        }
         return (
             <>
                 <div className="projectdetail-wrapper">
                     <h3 className="title-h3">{this.intrl.projinfo.info}</h3>
                     {
-                        this.props.molochinfo.projInfo.projDetail?<div className="detail-p" dangerouslySetInnerHTML={{ '__html': this.props.molochinfo.projInfo.projDetail }} />:<div className="detail-p">暂无详情</div> 
+                        (this.props.molochinfo.projInfo&&this.props.molochinfo.projInfo.projDetail)?<div className="detail-p" dangerouslySetInnerHTML={{ '__html': this.props.molochinfo.projInfo.projDetail }} />:<div className="detail-p">暂无详情</div> 
                     }                   
                 </div>
                 <div className="message-wrapper" id="message">
