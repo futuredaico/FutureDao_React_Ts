f870b778cffa7cee36bfb9c900c5a3118440b45d
dabanli123 <1031737848@qq.com>
2020-01-18 10:22:21 +0800

    更新路由配置

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -7,67 +7,39 @@ export default [
   //   path: '/address/:address',
   // },
   {
-    component: asyncComponent(() => import('../containers/proposal/molochdao')),
-    path: '/molochproposal/:projectId'
+    component: asyncComponent(() => import('../containers/home/home')),
+    path: '/index',
   },
   {
-    component: asyncComponent(() => import('../containers/proposal/futuredao')),
-    path: '/proposal/:projectId'
+    component: asyncComponent(() => import('../containers/proposal/molochdao/v1')),
+    path: '/proposalv1/:projectId',
   },
   {
-    component: asyncComponent(() => import('../containers/order/order')),
-    path: '/order/:projectId'
+    component: asyncComponent(() => import('../containers/proposal/molochdao/v2')),
+    path: '/proposalv2/:projectId',
+    children: [
+      {
+        path: '/proposalv2/token/:projectId',
+        component: asyncComponent(() => import('../containers/proposal/molochdao/v2/addtoken')),
+      },
+      {
+        path: '/proposalv2/kick/:projectId',
+        component: asyncComponent(() => import('../containers/proposal/molochdao/v2/kickmember')),
+      },
+      {
+        path: '/proposalv2/apply/:projectId',
+        component: asyncComponent(() => import('../containers/proposal/molochdao/v2/applyshares')),
+      },
+      {
+        path: '/proposalv2/menu/:projectId',
+        component: asyncComponent(() => import('../containers/proposal/molochdao/v2/menu')),
+      }
+    ]
   },
-  {
-    component: asyncComponent(() => import('../containers/order')),
-    path: '/giftorder/:projectId'
-  },
-  {
-    component: asyncComponent(() => import('../containers/support')),
-    path: '/support/:projectId'
-  },
-  {
-    component: asyncComponent(() => import('../containers/emailpage/invite')),
-    path: '/inviteteam',
-  },
-  {
-    component: asyncComponent(() => import('../containers/emailpage')),
-    path: '/email',
-  },
-  // {
-  //   component: asyncComponent(() => import('../containers/login')),
-  //   path: '/load',
-  //   children: [
-  //     {
-  //       component: asyncComponent(() => import('../containers/login/forget')),
-  //       path: '/load/forgetpwd',
-  //     },
-  //     {
-  //       component: asyncComponent(() => import('../containers/login/password')),
-  //       path: '/load/pwdupdate',
-  //     },
-  //     {
-  //       component: asyncComponent(() => import('../containers/login/signin')),
-  //       path: '/load/signin',
-  //     },
-  //     {
-  //       component: asyncComponent(() => import('../containers/login/login')),
-  //       path: '/load/login',
-  //     },
-  //   ]
-  // },
   {
     component: asyncComponent(() => import('../containers/personalcenter')),
     path: '/personalcenter',
-    children: [
-      {
-        path: '/personalcenter/myorder',
-        component: asyncComponent(() => import('../containers/personalcenter/myorder')),
-      },
-      {
-        path: '/personalcenter/myproject',
-        component: asyncComponent(() => import('../containers/personalcenter/myproject')),
-      },
+    children: [     
       {
         path: '/personalcenter/userinfo',
         component: asyncComponent(() => import('../containers/personalcenter/personedit')),
@@ -78,36 +50,6 @@ export default [
     component: asyncComponent(() => import('../containers/projectinfo/molochdao')),
     path: '/molochinfo/:projectId'
   },
-  {
-    component: asyncComponent(() => import('../containers/projectinfo/futuredao')),
-    path: '/projectinfo/:projectId'
-  },
-  // {
-  //   component: asyncComponent(() => import('../containers/project')),
-  //   path: '/project',
-  //   children: [
-  //     {
-  //       path: '/moloch/order/:projectId',
-  //       component: asyncComponent(() => import('../containers/project/molochdao/order/order')),
-  //     },
-  //     {
-  //       path: '/moloch/financing/:projectId',
-  //       component: asyncComponent(() => import('../containers/project/molochdao/financing/financing')),
-  //     },
-  //     {
-  //       path: '/moloch/update/:projectId',
-  //       component: asyncComponent(() => import('../containers/project/molochdao/updateproject')),
-  //     },
-  //     {
-  //       path: '/moloch/:projectId',
-  //       component: asyncComponent(() => import('../containers/project/molochdao/edit/createproject')),
-  //     },
-  //     {
-  //       path: '/project',
-  //       component: asyncComponent(() => import('../containers/project/molochdao/edit/createproject')),
-  //     }
-  //   ]
-  // },
   {
     component: asyncComponent(() => import('../containers/projectcreate')),
     path: '/create',
@@ -122,14 +64,6 @@ export default [
       }
     ]
   },
-  // {
-  //   component: asyncComponent(() => import('../containers/project')),
-  //   path: '/project/:projectId'
-  // },
-  // {
-  //   component: asyncComponent(() => import('../containers/project')),
-  //   path: '/project'
-  // },
   {
     component: asyncComponent(() => import('../containers/notfound')),
     path: '/:any',
